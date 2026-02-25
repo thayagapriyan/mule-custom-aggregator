f941f59779969bd309f6569725b3311e47b55478
thayagapriyan <rdsthayagan@gmail.com>
2026-02-25 14:16:08 -0600

    updated

--- src/main/java/com/priyan/aggregator/internal/MuleCustomAggregatorExtension.java
+++ src/main/java/com/priyan/aggregator/internal/MuleCustomAggregatorExtension.java
@@ -6,9 +6,13 @@ import org.mule.sdk.api.annotation.Extension;
 import org.mule.sdk.api.annotation.Configurations;
 import org.mule.sdk.api.annotation.dsl.xml.Xml;
 
+import org.mule.sdk.api.annotation.JavaVersionSupport;
+import org.mule.sdk.api.meta.JavaVersion;
+
 @Xml(prefix = "mule-custom-aggregator")
 @Extension(name = "Mule Custom Aggregator")
 @Configurations(MuleCustomAggregatorConfiguration.class)
+@JavaVersionSupport(JavaVersion.JAVA_17)
 public class MuleCustomAggregatorExtension {
 
 }
